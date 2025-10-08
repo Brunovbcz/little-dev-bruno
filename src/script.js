dc5b99644019ec0454b7d5265ec6681337bb3fdc
Brunovbcz <brunovbcz9@gmail.com>
2025-10-08 17:07:32 -0300

    v08/10

--- src/script.js
+++ src/script.js
@@ -5,10 +5,19 @@ function menuButton() {
     let title = document.querySelector('#title');
     let menuBtn = document.querySelector('#menu-btn')
     let textLabel = document.querySelectorAll('#text-label')
+    let background = document.querySelector('.background')
 
     if (title) title.classList.toggle('open')
     if (sideBar) sideBar.classList.toggle('open')
     if (menusBackground) menusBackground.classList.toggle('open')
     if (menuBtn) menuBtn.classList.toggle('open')
+    if (background) background.classList.toggle('open')
     if (textLabel) textLabel.forEach(function(t) {t.classList.toggle('open')})
+}
+
+// Função para abrir menu de adicionar equipamento
+function toggleEquipmentMenu() {
+    let background = document.querySelector('.all-background')
+
+    background.classList.toggle('visible')
 }
