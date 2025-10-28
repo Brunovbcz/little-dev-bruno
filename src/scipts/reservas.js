b1d1eb94a8d3dd52b79601eb8085e723c0dfcfe1
Brunovbcz <brunovbcz9@gmail.com>
2025-10-27 22:30:50 -0300

    v27/10

--- src/scipts/reservas.js
+++ src/scipts/reservas.js
@@ -1,5 +1,10 @@
 var equipmentsNames = []
 
+const nameInput = document.querySelector('#name')
+const eqInput = document.querySelector('.eq-list')
+const dateInput = document.querySelector('#limit-date')
+const obsInput = document.querySelector('#obs')
+
 // Pega os equipamentos cadastrados no servidor
 async function getEquipments() {
     try {
@@ -17,6 +22,7 @@ async function getEquipments() {
 document.addEventListener('DOMContentLoaded', async (e) => {
     let equipments = await getEquipments()
     
+    if (!equipments.equipamentosProntos) return
     equipments.equipamentosProntos.forEach((eq) => {
         equipmentsNames.push(eq.nome)
     })
@@ -31,13 +37,19 @@ function toggleAddReservation() {
 
     equipmentsNames.forEach(name => {
         const option = document.createElement('option')
-        option.value = name
-        datalist.appendChild(name)
+        option.textContent = name
+        datalist.appendChild(option)
     })
 }
 
 document.querySelector('.add-reservation-form').addEventListener('submit', (e) => {
     e.preventDefault()
     
+    let nameVal = nameInput.value
+    let optionVal = eqInput.options[eqInput.selectedIndex].text
+    let date = new Date()
+    let finalDate = dateInput.value
+    let obsVal = obsInput.value
 
+    console.log(nameVal, optionVal, date, finalDate, obsVal)
 })
